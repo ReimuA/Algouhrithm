2875cd6a33af283681707ea90e23d900c0d981ce
Thibault Dolley <thibault.dolley@epitech.eu>
2022-11-11 15:24:02 +0100

    Ajout powerset

---
+++ src/backtracking/powerset.ts
@@ -0,0 +1,32 @@
+// Assuming list does not contain duplicate
+function powerset(list: number[]): number[][] {
+    let results: number[][] = []
+    let subset: number[] = []
+
+    const makeSubset = (i: number) => {
+        if (!(i < list.length)) {
+            return results.push([...subset]);
+        }
+
+        subset.push(list[i]);
+        makeSubset(i + 1);
+
+        subset.pop();
+        makeSubset(i + 1);
+    }
+
+    makeSubset(0);
+    return results;
+}
+
+
+let demo = () => {
+    let set = [1, 2, 3, 4];
+    let res = powerset([1, 2, 3, 4]);
+
+    console.log(`Powerset of ${set}`);
+    for (const l of res) console.log(l);
+}
+
+
+export default demo;
