2bd90d99abe64c60ca59fc57346554535e8c47dc
Thibault Dolley <thibault.dolley@epitech.eu>
2022-11-13 15:54:45 +0100

    Added color

--- src/raymarching/core/core.ts
+++ src/raymarching/core/core.ts
@@ -17,17 +17,18 @@ export type ViewPort = {
 
 export interface RaymarchObject {
     sdf(otherPos: Vector3): number;
+    normal(otherPos: Vector3): Vector3;
 }
 
 export async function raymarch(objects: RaymarchObject[]) {
     const viewPort = {
-        width: 512,
-        height: 512,
+        width: 540,
+        height: 540,
         pos: new Vector3(0),
         eye: new Vector3(0, 0, -100)
     };
 
-    const minDistance = 0.000001;
+    const minDistance = 0.01;
     const maxDepth = 1000;
     const maxStep = 1000;
     const buffer: Color[] = [];
@@ -43,17 +44,25 @@ export async function raymarch(objects: RaymarchObject[]) {
             let depth = 0;
 
             while (step < maxStep && depth < maxDepth) {
-                const rayPos = new Vector3(rayDirection.x * depth, rayDirection.y * depth, rayDirection.z * depth);
+                const rayPos = rayDirection.mulScalar(depth);
                 const dist = objects[0].sdf(rayPos);
 
-                if (dist <= minDistance) break;
+                if (dist <= minDistance) {
+                    const normal = objects[0].normal(rayPos).normalize();
 
+                    buffer.push({
+                        r: Math.abs(normal.x * 255),
+                        g: Math.abs(normal.y * 255),
+                        b: Math.abs(normal.z * 255),
+                        a: 255
+                    });
+                    break;
+                }
                 depth += dist;
                 step++;
             }
 
             if (depth >= maxDepth || step >= maxStep) buffer.push({ r: 0, g: 0, b: 0, a: 255 });
-            else buffer.push({ r: 255, g: 0, b: 0, a: 255 });
         }
     }
     await exportBufferToPng(viewPort.width, viewPort.height, buffer);
--- src/raymarching/sphere.ts
+++ src/raymarching/sphere.ts
@@ -4,21 +4,31 @@ import { raymarch, RaymarchObject } from './core/core';
 class Sphere implements RaymarchObject {
     constructor(pos: Vector3, size: number) {
         this.pos = pos;
-        this.size = size;
+        this.x = pos.x;
+        this.y = pos.y;
+        this.z = pos.z;
+        this.radius = size;
     }
 
     public pos: Vector3;
-    public size: number;
+    public x: number;
+    public y: number;
+    public z: number;
+    public radius: number;
 
     sdf(otherPos: Vector3): number {
-        return this.pos.distanceTo(otherPos) - this.size;
+        return this.pos.distanceTo(otherPos) - this.radius;
+    }
+
+    normal(pos: Vector3): Vector3 {
+        return pos.sub(this.pos).normalize();
     }
 }
 
 const demo = () => {
-    const sphere = new Sphere(new Vector3(0, 0, 100), 70);
+    const sphere = new Sphere(new Vector3(0, 0, 800), 700);
 
-    console.log(`Rendering sphere at ${sphere.pos} with size ${sphere.size}}`);
+    console.log(`Rendering sphere at ${sphere.pos} with size ${sphere.radius}}`);
 
     raymarch([sphere]);
     console.log('check output.png');
