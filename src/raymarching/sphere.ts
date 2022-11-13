35c7381bc882064ffc56d80fb01a0097f40446df
Thibault Dolley <thibault.dolley@epitech.eu>
2022-11-13 13:16:05 +0100

    Update to sphere to render image on demo

--- src/raymarching/sphere.ts
+++ src/raymarching/sphere.ts
@@ -1,6 +1,7 @@
 import { Vector3 } from 'math3d';
+import { raymarch, RaymarchObject } from './core/core';
 
-class Sphere {
+class Sphere implements RaymarchObject {
     constructor(pos: Vector3, size: number) {
         this.pos = pos;
         this.size = size;
@@ -15,11 +16,13 @@ class Sphere {
 }
 
 const demo = () => {
-    const sphere = new Sphere(new Vector3(10, 0, 0), 1);
+    const sphere = new Sphere(new Vector3(0, 0, 75), 30);
 
     console.log(
         `Distance to sphere at ${sphere.pos} with size ${sphere.size} from 0: ${sphere.sdf(new Vector3(0, 0, 0))}`
     );
+
+    raymarch([sphere]);
 };
 
 export default demo;
