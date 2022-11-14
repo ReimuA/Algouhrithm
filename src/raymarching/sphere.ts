9f79f047d14704fd416a69d020706946040f011d
Thibault Dolley <thibault.dolley@epitech.eu>
2022-11-14 15:08:25 +0100

    Dead code

--- src/raymarching/sphere.ts
+++ src/raymarching/sphere.ts
@@ -4,16 +4,10 @@ import { raymarch, RaymarchObject } from './core/core';
 class Sphere implements RaymarchObject {
     constructor(pos: Vector3, size: number) {
         this.pos = pos;
-        this.x = pos.x;
-        this.y = pos.y;
-        this.z = pos.z;
         this.radius = size;
     }
 
     public pos: Vector3;
-    public x: number;
-    public y: number;
-    public z: number;
     public radius: number;
 
     sdf(otherPos: Vector3): number {
@@ -28,7 +22,7 @@ class Sphere implements RaymarchObject {
 const demo = () => {
     const sphere = new Sphere(new Vector3(0, 0, 800), 700);
 
-    console.log(`Rendering sphere at ${sphere.pos} with size ${sphere.radius}}`);
+    console.log(`Rendering sphere at ${sphere.pos} with size ${sphere.radius}`);
 
     raymarch([sphere]);
     console.log('check output.png');
